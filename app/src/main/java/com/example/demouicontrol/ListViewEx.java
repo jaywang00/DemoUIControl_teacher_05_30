fc36243b5ed6b30677c3fd085549b96a472e1b5f
Jay Wang <ielcw00@gmail.com>
2018-06-06 09:22:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demouicontrol/ListViewEx.java
@@ -0,0 +1,31 @@
+package com.example.demouicontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ListViewEx extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    String[] array = {"Meal", "Vag", "Both_OK"};
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview);
+        listView = findViewById(R.id.listView1);
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,array);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this,((TextView)view).getText().toString(),Toast.LENGTH_SHORT).show();
+    }
+}
