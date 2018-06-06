c9cb67f3f8e276cb33cbd08821659820d569a83e
Jay Wang <ielcw00@gmail.com>
2018-06-06 11:23:20 +0800

    v1.2_Multiple-Choice_Checked

--- app/src/main/java/com/example/demouicontrol/ListViewEx.java
+++ app/src/main/java/com/example/demouicontrol/ListViewEx.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,7 +20,7 @@ public class ListViewEx extends AppCompatActivity implements AdapterView.OnItemC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
         listView = findViewById(R.id.listView1);
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_single_choice,array);
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_multiple_choice,array);
         listView.setAdapter(adapter);
 //        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         listView.setOnItemClickListener(this);
@@ -27,6 +28,10 @@ public class ListViewEx extends AppCompatActivity implements AdapterView.OnItemC
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this,((TextView)view).getText().toString(),Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this,((TextView)view).getText().toString(),Toast.LENGTH_SHORT).show();
+
+        CheckedTextView ctv = (CheckedTextView)view;
+        if(ctv.isChecked())
+            Toast.makeText(this,((TextView)view).getText().toString(),Toast.LENGTH_SHORT).show();
     }
 }
